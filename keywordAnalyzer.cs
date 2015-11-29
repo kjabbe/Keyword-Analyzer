369876cf53a479f5544fbb66139b69548cf25efa
kjabbe <kjabbe@gmail.com>
2015-11-29 20:33:18 +0100

    generating objects of each uniqe word+counter

--- keywordAnalyzer.cs
+++ keywordAnalyzer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace KeywordAnalyzer 
 {
@@ -8,9 +9,13 @@ namespace KeywordAnalyzer
 	{
 		public static void Main()
 		{
-			//string link_word_file = @"linkwords_english.txt";
-			//List<string> linking_words = linkingWords(link_word_file);
+			string link_word_file = @"linkwords_english.txt";
+			List<string> linking_words = linkingWords(link_word_file);
 			//createLinkwordList("words.txt", "linkwords_english.txt");
+			List<Word> eksamen2011 = textParser("eksamen2011.txt", linking_words);
+			List<Word> eksamen2012 = textParser("eksamen2012.txt", linking_words);
+			//List<Word> eksamen2011 = textParser("eksamen2011.txt", linking_words);
+			compare(eksamen2011, eksamen2012);
 		}
 		public static List<string> linkingWords(string path)
 		{
@@ -20,8 +25,65 @@ namespace KeywordAnalyzer
 			while((line = file.ReadLine()) != null)
 			{
 				linkWords.Add(line);
+				//Console.WriteLine(line);
 			}
 			return linkWords;
+		}
+		
+		public static List<Word> textParser(string fileName, List<string> link_words)
+		{
+			string line;
+			StreamReader file = new StreamReader(fileName);
+			List<Word> lWord = new List<Word>();
+			
+			while((line = file.ReadLine()) != null)
+			{
+				line = line.ToLower();
+				string[] line_array = line.Split(' ');
+				foreach (string w in line_array)
+				{
+					string sanitized = sanitizeString(w);
+					//Console.WriteLine(sanitized);
+					//var match = link_words.Contains(sanitized);
+					//Console.WriteLine("match {0} , with {1}",match.ToString(), sanitized);
+					if (link_words.Contains(sanitized) == false && sanitized != null){
+						var res = lWord.Find(word => word.name == sanitized);
+						if (res == null)
+						{
+							Word word = new Word();
+							word.name = sanitized;
+							word.counter = 1;
+							word.context = line;
+							lWord.Add(word);
+						} else {
+							res.counter++;
+						}
+					}
+				}			
+			}
+			foreach (Word w in lWord){
+				//Console.WriteLine("{0} funnet {1} ganger", w.name, w.counter);
+				Console.WriteLine("{0} - {1}", w.name, w.counter);
+				//Console.WriteLine("Context: '{0}'", w.context);
+			}
+			return lWord;
+		}
+		
+		public static string sanitizeString (string str )
+		{
+			List<string> patterns = new List<string>() {"\\p{Sc}", "\\%", "\\.", "\\,", "\\?", "\\(", "\\)", "\\d+"};
+			foreach (string pattern in patterns)
+			{
+				Regex rgx = new Regex(pattern);
+				str = rgx.Replace(str, "");
+				//Console.Write(str + " ");
+			}
+			//Console.WriteLine();
+			//Console.WriteLine("returned {0}",str);
+			if (str.Length <= 1)
+				return null;
+			else
+				return str;
 		}
 		
 		public static void createLinkwordList(string path, string outputFilename)
@@ -49,9 +111,10 @@ namespace KeywordAnalyzer
 		
 	}
 	
-	public class Words 
+	public class Word 
 	{
-		public string word { get; set; }
-		public int counter { get; set; }	
+		public string name { get; set; }
+		public int counter { get; set; }
+		public string context { get; set; }	
 	}
 }
